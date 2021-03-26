561832fdd2a900a70ed048f97303ce407231f0ef
Aleksey Kritskov <krickov.aleksey@cadmean.ru>
2021-03-26 18:43:17 +0300

    Initial Commit

---
+++ SpeechForUnityObjc/SpeechForUnityObjc.h
@@ -0,0 +1,18 @@
+//
+//  SpeechForUnityObjc.h
+//  SpeechForUnityObjc
+//
+//  Created by Алексей Крицков on 26.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpeechForUnityObjc.
+FOUNDATION_EXPORT double SpeechForUnityObjcVersionNumber;
+
+//! Project version string for SpeechForUnityObjc.
+FOUNDATION_EXPORT const unsigned char SpeechForUnityObjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpeechForUnityObjc/PublicHeader.h>
+
+
